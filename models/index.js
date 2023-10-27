4f10ceaca772fec4a705feb7d4279368f1ba8b64
mbman11 <m.behrmann11@gmail.com>
2023-10-26 18:55:06 -0600

    added new changes, commented out .hasmany etc

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const Users = require('./Users');
 const Blog = require('./Blog');
 
-Users.hasMany(Blog, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+// Users.hasMany(Blog, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
-Blog.belongsTo(Users, {
-  foreignKey: 'user_id'
-});
+// Blog.belongsTo(Users, {
+//   foreignKey: 'user_id'
+// });
 
 module.exports = { Users, Blog };
