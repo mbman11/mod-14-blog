3a5dce19b82be6e83215aebfb94571e366dbcec7
mbman11 <m.behrmann11@gmail.com>
2023-10-26 21:33:20 -0600

    changed conn.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,32 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+// module.exports = sequelize;
+
+
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+// create connection to our db
+const sequelize = process.env.MYSQL_URL
+  ? new Sequelize(process.env.MYSQL_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
 
-
-// mysql -u root -p //
-// npm run seeds //
-// node server.js //
