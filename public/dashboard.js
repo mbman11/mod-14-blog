b7b553f6eef9cb82921b9a36add828dfc7fb9a88
mbman11 <m.behrmann11@gmail.com>
2023-10-26 21:51:31 -0600

    added stuff

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,10 +1,11 @@
+const postId = '';
 
 
 const deleteBlogBtn = document.getElementById('deleteButton');
 
 deleteBlogBtn.addEventListener("click",function(){
   console.log('hello')
-  fetch(`/api/blogs/delete/:id`,  {
+  fetch(`/api/blogs/delete/${postId}`,  {
     method: 'DELETE'
   })
 })
