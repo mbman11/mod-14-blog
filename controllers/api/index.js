818a387f5c7df695b4cdbad8f51f4cec4efdccdd
mbman11 <m.behrmann11@gmail.com>
2023-10-26 19:02:21 -0600

    added .js to api/index.js require paths

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const blogRoutes = require('./blog-routes');
+const userRoutes = require('./user-routes.js');
+const blogRoutes = require('./blog-routes.js');
 // const userRoutes = require('./');
 
 router.use('/blogs', blogRoutes);
